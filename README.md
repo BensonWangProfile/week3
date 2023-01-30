22560b72ce68e76fd03a0c3320af42330582160b
benson <benson95217@gmail.com>
2023-01-30 16:49:00 +0800

    Update 2023-01-30T08:48:57.496Z

--- README.md
+++
@@ -1,6 +0,0 @@
-# Week3 Sweetaste
-
-## 使用套件
-- gulp
-  - 載入時必須注意node版本（< v14）
-- tailwind
---
+++ assets/js/all.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// toggle button
+var toggleBtn = document.querySelector("#toggleBtn");
+var menu = document.querySelector(".navbar"); //要用class才可以用classList
+
+toggleBtn.addEventListener("click", function () {
+  menuToggle();
+});
+
+function menuToggle() {
+  menu.classList.toggle("top-[84px]");
+}
+//# sourceMappingURL=all.js.map
