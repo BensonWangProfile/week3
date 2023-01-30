754ae5b67f676187b13043ecf08e9ecd09b1128f
benson <benson95217@gmail.com>
2023-01-30 16:38:22 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,12 @@
+// toggle button
+const toggleBtn = document.querySelector("#toggleBtn");
+const menu = document.querySelector(".navbar");  //要用class才可以用classList
+
+toggleBtn.addEventListener("click", function(){
+  menuToggle();
+});
+
+function menuToggle(){
+  menu.classList.toggle("top-[84px]")
+}
+
